2d6678d3c5141aa3b1d41728e0ef882ac812dad5
itggot-gabriella-vacante <gabriella.vacante@elev.ga.ntig.se>
2018-12-28 23:03:53 +0100

    Sketches, first page, timer
    Moved sketches to the right map, edited the first page in html and css and created a timer in i.g. js

---
+++ site/js/first_page.js
@@ -0,0 +1,17 @@
+
+
+
+var countDownDate = new Date("dec 24, 2019 00:00:00").getTime();
+var countDownfunction = setInterval(function() {
+    var now = new Date().getTime();
+    var distance = countDownDate - now;
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    document.getElementById("demo").innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+    if(distance < 0) {
+        clearInterval(countDownfunction);
+        document.getElementById("demo").innerHTML - "It's Christmas!";
+    }
+}, 1000);
