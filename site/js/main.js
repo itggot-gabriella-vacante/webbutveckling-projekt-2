dbcf0316617a2a17d6c9c6afccd87efb7ff0a2dc
itggot-gabriella-vacante <gabriella.vacante@elev.ga.ntig.se>
2019-01-24 22:51:44 +0100

    Menu, background picture to link, text in header
    Code to make the menu work in js, css and html, styling of the menu and the menu button in css, added an icon to the menu, background picture to a future link, changing and adding code in html to center the text in the header, made text in header a link and styled it

--- site/js/main.js
+++ site/js/main.js
@@ -186,3 +186,10 @@ var countDownfunction = setInterval(function() {
 }, 1000);
 
 /*Menu*/
+function showMenu(event){
+  let menu = document.querySelector(".menu")
+  menu.classList.toggle("show")
+
+  let fadeLayer = document.querySelector(".fade-layer")
+  fadeLayer.classList.toggle("visible")
+}
